c1029c9fbbfa1faceb9aa1414e0416a4a6c6188f
anubra266 <anubra266@gmail.com>
2020-10-28 13:04:05 +0100

    ignore ziggy js

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-//TODO Sync Settings
+//TODO Sync VsCode Settings
 Route::group(['middleware' => ['guest']], function () {
     //?Index route
     Route::get('/', 'PublicController@landing')->name('landing');
